b4d6ce7374fad0de0dbab127e23929c5c85d611b
cristina <yosola@gmail.com>
2018-08-07 12:52:33 -0700

    Adding user component working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 import './App.css';
 
 // Initialize Firebase
@@ -20,15 +21,21 @@ class App extends Component {
     super(props);
 
     this.state={
-      activeRoom:""
+      activeRoom:"",
+      activeUser:""
     }
 
     this.setActiveRoom = this.setActiveRoom.bind(this);
+    this.setUser = this.setUser.bind(this);
   }
 
   setActiveRoom(room) {
       this.setState({ activeRoom: room })
       console.log(this.state.activeRoom);
+    }
+
+  setUser(user) {
+      this.setState({ activeUser: user })
     }
 
   render() {
@@ -42,6 +49,11 @@ class App extends Component {
           setActiveRoom={this.setActiveRoom}/>
         </div>
         <div className="main">
+        <User
+        firebase={firebase}
+        setUser={this.setUser}
+        user={this.state.activeUser}
+        />
         <MessageList
         firebase={firebase}
         activeRoom={this.state.activeRoom}/>
